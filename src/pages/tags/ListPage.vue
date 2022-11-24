85962cc009a5249755102290fc00e15a74d4a282
Alessandro Sales <contato@alessandrosales.com.br>
2022-11-24 08:42:58 -0300

    refactor(tags): Quebramos as páginas em componentes

--- src/pages/tags/ListPage.vue
+++ src/pages/tags/ListPage.vue
@@ -3,83 +3,10 @@
     <default-container title="Lista de Tags">
       <div class="row q-col-gutter-md">
         <div class="col-12">
-          <q-input
-            label="Pesquisa"
-            v-model="search"
-            placeholder="Pesquise por nome"
-            outlined
-          />
-        </div>
-        <div class="col-12 text-right">
-          <q-btn
-            icon="add"
-            label="Adicionar"
-            @click="goToRecord()"
-            color="primary"
-          />
+          <tag-filter @filtrate="filtrate" />
         </div>
         <div class="col-12">
-          <q-markup-table flat bordered>
-            <thead>
-              <tr>
-                <th class="text-center" width="1%">Opções</th>
-                <th class="text-center">Nome</th>
-                <th class="text-center">Cor</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="text-center">
-                  <q-btn
-                    icon="edit"
-                    @click="goToRecord(1)"
-                    color="primary"
-                    class="q-mr-sm"
-                    size="10px"
-                    round
-                    outline
-                  />
-                  <q-btn
-                    icon="delete"
-                    @click="remove(1)"
-                    color="red"
-                    size="10px"
-                    round
-                    outline
-                  />
-                </td>
-                <td class="text-center">Nome da tag</td>
-                <td class="text-center">
-                  <q-chip icon="tag" label="red" color="red" />
-                </td>
-              </tr>
-              <tr>
-                <td class="text-center">
-                  <q-btn
-                    icon="edit"
-                    @click="goToRecord(2)"
-                    color="primary"
-                    class="q-mr-sm"
-                    size="10px"
-                    round
-                    outline
-                  />
-                  <q-btn
-                    icon="delete"
-                    @click="remove(2)"
-                    color="red"
-                    size="10px"
-                    round
-                    outline
-                  />
-                </td>
-                <td class="text-center">Nome da tag</td>
-                <td class="text-center">
-                  <q-chip icon="tag" label="blue" color="blue" />
-                </td>
-              </tr>
-            </tbody>
-          </q-markup-table>
+          <tag-table :records="records" />
         </div>
       </div>
     </default-container>
@@ -88,40 +15,25 @@
 
 <script lang="ts">
 import { defineComponent, ref } from 'vue';
-import { useQuasar } from 'quasar';
-
-import { useRouter } from 'vue-router';
 
 import DefaultContainer from 'src/components/shared/DefaultContainer.vue';
+import TagTable from 'src/components/tags/TagTable.vue';
+import TagFilter from 'src/components/tags/TagFilter.vue';
 
 export default defineComponent({
-  components: { DefaultContainer },
+  components: { DefaultContainer, TagTable, TagFilter },
   setup() {
-    const q = useQuasar();
-    const search = ref();
+    const records = ref([
+      { id: 1, name: 'Tag 1', color: 'blue' },
+      { id: 2, name: 'Tag 2', color: 'red' },
+      { id: 3, name: 'Tag 3', color: 'yellow' },
+    ]);
 
-    const router = useRouter();
-
-    function goToRecord(id?: number) {
-      if (id) {
-        router.push({ name: 'tag', params: { id } });
-        return;
-      }
-      router.push({ name: 'tag' });
+    function filtrate(search: string) {
+      console.log(search);
     }
 
-    function remove(id: number) {
-      q.dialog({
-        title: 'Excluir Registro',
-        message: 'Tem certeza que você quer prosseguir?',
-        cancel: true,
-        persistent: true,
-      }).onOk(() => {
-        console.log('Ok, vamos excluir este registro:', id);
-      });
-    }
-
-    return { search, goToRecord, remove };
+    return { records, filtrate };
   },
 });
 </script>
